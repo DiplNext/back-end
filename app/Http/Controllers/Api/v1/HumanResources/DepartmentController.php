cacaf8e7daa76587dcd49ff98551e5dcd0b27965
diplnext <eugenispas15@gmail.com>
2022-08-31 20:26:33 +0300

    feat: add crud operation to controller

--- app/Http/Controllers/Api/v1/HumanResources/DepartmentController.php
+++ app/Http/Controllers/Api/v1/HumanResources/DepartmentController.php
@@ -3,64 +3,66 @@
 namespace App\Http\Controllers\Api\v1\HumanResources;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\DepartmentRequest;
 use App\Http\Resources\DepartmentResource;
 use App\Models\Department;
-use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Http\Response;
 
 class DepartmentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return AnonymousResourceCollection
-     */
+  /**
+   * Display a listing of the resource.
+   *
+   * @return AnonymousResourceCollection
+   */
     public function index(): AnonymousResourceCollection
     {
         return DepartmentResource::collection(Department::all());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param Request $request
-     * @return Response
-     */
-    public function store(Request $request)
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param DepartmentRequest $request
+   * @return DepartmentResource
+   */
+    public function store(DepartmentRequest $request): DepartmentResource
     {
-        //
+        $department = Department::create($request->validated());
+        return DepartmentResource::make($department);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param Department $department
-     * @return DepartmentResource
-     */
-    public function show(Department $department)
+  /**
+   * Display the specified resource.
+   *
+   * @param Department $department
+   * @return DepartmentResource
+   */
+    public function show(Department $department): DepartmentResource
     {
         return DepartmentResource::make($department);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param Request $request
-     * @param Department $department
-     * @return Response
-     */
-    public function update(Request $request, Department $department)
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param DepartmentRequest $request
+   * @param Department $department
+   * @return DepartmentResource
+   */
+    public function update(DepartmentRequest $request, Department $department): DepartmentResource
     {
-        //
+        $department->update($request->validated());
+        return DepartmentResource::make($department);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param Department $department
-     * @return int
-     */
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param Department $department
+   * @return Response
+   */
     public function destroy(Department $department): int
     {
         $department->delete();
--- app/Http/Controllers/Api/v1/HumanResources/DepartmentTypeController.php
+++ app/Http/Controllers/Api/v1/HumanResources/DepartmentTypeController.php
@@ -3,64 +3,66 @@
 namespace App\Http\Controllers\Api\v1\HumanResources;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\DepartmentTypeRequest;
 use App\Http\Resources\DepartmentTypeResource;
 use App\Models\DepartmentType;
-use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Http\Response;
 
 class DepartmentTypeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return AnonymousResourceCollection
-     */
+  /**
+   * Display a listing of the resource.
+   *
+   * @return AnonymousResourceCollection
+   */
     public function index(): AnonymousResourceCollection
     {
         return DepartmentTypeResource::collection(DepartmentType::withTrashed()->get());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param Request $request
-     * @return Response
-     */
-    public function store(Request $request)
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param DepartmentTypeRequest $request
+   * @return DepartmentTypeResource
+   */
+    public function store(DepartmentTypeRequest $request): DepartmentTypeResource
     {
-        //
+        $departmentType = DepartmentType::create($request->validated());
+        return DepartmentTypeResource::make($departmentType);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param DepartmentType $departmentType
-     * @return DepartmentTypeResource
-     */
+  /**
+   * Display the specified resource.
+   *
+   * @param DepartmentType $departmentType
+   * @return DepartmentTypeResource
+   */
     public function show(DepartmentType $departmentType): DepartmentTypeResource
     {
         return DepartmentTypeResource::make($departmentType);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param Request $request
-     * @param DepartmentType $departmentType
-     * @return Response
-     */
-    public function update(Request $request, DepartmentType $departmentType)
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param DepartmentTypeResource $request
+   * @param DepartmentType $departmentType
+   * @return DepartmentTypeResource
+   */
+    public function update(DepartmentTypeResource $request, DepartmentType $departmentType): DepartmentTypeResource
     {
-        //
+        $departmentType->update($request->validated());
+        return DepartmentTypeResource::make($departmentType);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param DepartmentType $departmentType
-     * @return int
-     */
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param DepartmentType $departmentType
+   * @return int
+   */
     public function destroy(DepartmentType $departmentType): int
     {
         $departmentType->delete();
